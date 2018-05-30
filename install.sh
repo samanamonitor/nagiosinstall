8149d1ba08b1bdd014b44c9658ae56506977f0db
Fabian Baena <fabbaena@gmail.com>
2018-05-30 13:08:00 -0400

    added lxc install, network config and nginx config

--- install.sh
+++ install.sh
@@ -1,28 +1,86 @@
 #!/bin/sh
 
-HOST=nagiost1
-H_IP=169.254.254.10
+CONTAINER=nagiost1
+C_IP=169.254.254.10
+C_NETMASK=255.255.255.0
+C_GATEWAY=169.254.254.1
+C_PATH=/var/lib/lxc/$CONTAINER/rootfs
+C_FQDN=nagios.smyt1.gslb.ncl.com
+H_IP=169.254.254.1
 H_NETMASK=255.255.255.0
-H_GATEWAY=169.254.254.1
-H_PATH=/var/lib/lxc/$HOST/rootfs
-echo "Creating Container $HOST ..."
-lxc-create -t centos -n $HOST
-echo "Container $HOST created."
-echo "Configuring IP Address $H_IP ..."
-cat <<EOT > $H_PATH/etc/sysconfig/network-scripts/ifcfg-eth0
-DEVICE=eth0
-BOOTPROTO=static
-IPADDR=$H_IP
-NETMASK=$H_NETMASK
-GATEWAY=$H_GATEWAY
-ONBOOT=yes
-HOSTNAME=$HOST
-NM_CONTROLLED=no
-TYPE=Ethernet
-MTU=
-EOT
-echo "IP Address Configured."
-echo "$H_IP $HOST" >> /etc/hosts
+
+install_lxc() {
+   if ! rpm -ql lxc >/dev/null; then
+      yum -y install lxc lxc-templates lxc-extra
+      systemctl enable lxc.service
+      echo "Installed LXC containers."
+   fi
+}
+
+enable_ipforward() {
+   if ! grep -e "^net.ipv4.ip_forward\s*=\s*1" /etc/sysctl.conf >/dev/null; then 
+      echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+      echo "Enabled IP Forwarding."
+   fi
+}
+
+set_ip() {
+   NAME=$1
+   FPATH=$2
+   TYPE=$3
+   IPADDR=$4
+   NETMASK=$5
+   GATEWAY=$6
+
+   if [ ! -z $GATEWAY ]; then
+      $GATEWAY="GATEWAY=${GATEWAY}"
+   fi
+
+   cat include/ifcfg.template | \
+      sed -e "s|\%NAME\%|${NAME}|" \
+          -e "s|\%TYPE\%|${TYPE}|" \
+          -e "s|\%IPADDR\%|${IPADDR}|" \
+          -e "s|\%NETMASK\%|${NETMASK}|" \
+          -e "s|\%GATEWAY\%|${GATEWAY}|" \
+       > ${FPATH}/ifcfg-${NAME}
+   echo "Interface ${NAME} has been configured."
+}
+
+setup_firewall() {
+   IFCONTAINERS=$1
+   IFOUT=$2
+
+   firewall-cmd --permanent --zone=internal --change-interface=$IFCONTAINERS
+   firewall-cmd --direct --add-rule ipv4 nat POSTROUTING 0 -o $IFOUT -j MASQUERADE
+   firewall-cmd --direct --add-rule ipv4 filter FORWARD 0 -i $IFCONTAINERS -o $IFOUT -j ACCEPT
+   firewall-cmd --direct --add-rule ipv4 filter FORWARD 0 -o $IFCONTAINERS -i $IFOUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+   firewall-cmd --reload
+   echo "Firewall has been configured."
+}
+
+setup_host_network() {
+   IFCONTAINERS=$1
+   IFOUT=$2
+   HOSTIP=$3
+   HOSTMASK=$4
+
+   if [ ! -f /etc/sysconfig/network-scripts/ifcfg-${IFCONTAINERS} ]; then
+      set_ip $IFCONTAINERS /etc/sysconfig/network-scripts Bridge $HOSTIP $HOSTMASK
+      setup_firewall $IFCONTAINERS $IFOUT
+   fi
+}
+
+install_lxc
+enable_ipforward
+setup_host_network virbr0 ens160 $H_IP $H_NETMASK
+ifup virbr0
+
+echo "Creating Container $CONTAINER ..."
+lxc-create -t centos -n $CONTAINER
+echo "Container $CONTAINER created."
+echo "Configuring container IP address $C_IP ..."
+set_ip eth0 $C_PATH/etc/sysconfig/network-scripts/ Ethernet $C_IP $C_NETMASK $C_GATEWAY
+echo "$C_IP $CONTAINER" >> /etc/hosts
 if [ ! -d ~/.ssh ]; then
     mkdir ~/.ssh
     chmod 700 ~/.ssh
@@ -31,49 +89,54 @@ if [ ! -f ~/.ssh/id_rsa ]; then
     ssh-keygen -t rsa -C "nagios@samanagroup.com" -f ~/.ssh/id_rsa -P ""
     echo "Created SSH keys."
 fi
-if [ ! -d "$H_PATH/root/.ssh" ]; then
-    mkdir $H_PATH/root/.ssh
-    chmod 700 $H_PATH/root/.ssh
+if [ ! -d "$C_PATH/root/.ssh" ]; then
+    mkdir $C_PATH/root/.ssh
+    chmod 700 $C_PATH/root/.ssh
     echo "Created root .ssh path."
 fi
-cat ~/.ssh/id_rsa.pub >> $H_PATH/root/.ssh/authorized_keys
-chmod 600 $H_PATH/root/.ssh/authorized_keys
+cat ~/.ssh/id_rsa.pub >> $C_PATH/root/.ssh/authorized_keys
+chmod 600 $C_PATH/root/.ssh/authorized_keys
 echo "Created authorized_keys."
 if ! dig mirrors.fedoraproject.org | grep "NOERROR">/dev/null; then
-    echo `dig +short mirrors.fedoraproject.org @8.8.8.8 | sed -n 2p` mirrors.fedoraproject.org >> $H_PATH/etc/hosts
+    echo `dig +short mirrors.fedoraproject.org @8.8.8.8 | sed -n 2p` mirrors.fedoraproject.org >> $C_PATH/etc/hosts
     echo "WARNING: DNS is not able to resove mirrors.fedoraproject.org. Creating a hosts entry."
 fi
-lxc-start -n $HOST -d
+lxc-start -n $CONTAINER -d
 echo "Started Container"
 echo "Starting to install packages..."
-lxc-attach -n $HOST -- /usr/bin/yum -y install epel-release git
-lxc-attach -n $HOST -- /usr/bin/yum -y install nagios nagios-plugins nagios-plugins-ping nagios-plugins-users nagios-plugins-load nagios-plugins-http nagios-plugins-disk nagios-plugins-ssh nagios-plugins-swap nagios-plugins-procs
-lxc-attach -n $HOST -- /usr/bin/yum -y install pnp4nagios
-mkdir -p $H_PATH/var/www/html
-mkdir -p $H_PATH/var/log/httpd
+lxc-attach -n $CONTAINER -- /usr/bin/yum -y install epel-release git
+lxc-attach -n $CONTAINER -- /usr/bin/yum -y install nagios nagios-plugins nagios-plugins-ping nagios-plugins-users nagios-plugins-load nagios-plugins-http nagios-plugins-disk nagios-plugins-ssh nagios-plugins-swap nagios-plugins-procs
+lxc-attach -n $CONTAINER -- /usr/bin/yum -y install pnp4nagios
+mkdir -p $C_PATH/var/www/html
+mkdir -p $C_PATH/var/log/httpd
 echo "Packages installed."
-lxc-attach -n $HOST -- systemctl enable httpd
-lxc-attach -n $HOST -- systemctl start httpd
+lxc-attach -n $CONTAINER -- systemctl enable httpd
+lxc-attach -n $CONTAINER -- systemctl start httpd
 echo "HTTPD daemon started"
-cp include/index.html $H_PATH/var/www/html/index.html
+cp include/index.html $C_PATH/var/www/html/index.html
 
 ##
 #### Configure and Start Nagios #####
 ##
 
-mv $H_PATH/etc/nagios/nagios.cfg $H_PATH/etc/nagios/nagios.cfg.orig
-cp include/nagios.cfg $H_PATH/etc/nagios/nagios.cfg
+mv $C_PATH/etc/nagios/nagios.cfg $C_PATH/etc/nagios/nagios.cfg.orig
+cp include/nagios.cfg $C_PATH/etc/nagios/nagios.cfg
 
-mkdir -p $H_PATH/etc/nagios/objects/samana
-cp include/perfdata.cfg $H_PATH/etc/nagios/objects/samana/perfdata.cfg
-cp include/commands.cfg $H_PATH/etc/nagios/objects/samana/commands.cfg
+mkdir -p $C_PATH/etc/nagios/objects/samana
+cp include/perfdata.cfg $C_PATH/etc/nagios/objects/samana/perfdata.cfg
+cp include/commands.cfg $C_PATH/etc/nagios/objects/samana/commands.cfg
 
-lxc-attach -n $HOST -- systemctl enable nagios
-lxc-attach -n $HOST -- systemctl start nagios
+lxc-attach -n $CONTAINER -- systemctl enable nagios
+lxc-attach -n $CONTAINER -- systemctl start nagios
 echo "Nagios has been configured and started."
 
-mv $H_PATH/etc/pnp4nagios/npcd.cfg $H_PATH/etc/pnp4nagios/npcd.cfg.orig
-cp include/npcd.cfg $H_PATH/etc/pnp4nagios/npcd.cfg
-lxc-attach -n $HOST -- systemctl enable npcd
-lxc-attach -n $HOST -- systemctl start npcd
+mv $C_PATH/etc/pnp4nagios/npcd.cfg $C_PATH/etc/pnp4nagios/npcd.cfg.orig
+cp include/npcd.cfg $C_PATH/etc/pnp4nagios/npcd.cfg
+lxc-attach -n $CONTAINER -- systemctl enable npcd
+lxc-attach -n $CONTAINER -- systemctl start npcd
 echo "PNP4Nagios has been configured and started."
+
+/usr/bin/cat include/nginx_nagios.conf.template | \
+         sed -e "s|\%SERVER\%|${HOST}|" \
+             -e "s|\%SERVER_IP\%|${C_IP}|" \
+             -e "s|%FQDN%|${C_FQDN}|" > /etc/nginx/conf.d/${HOST}.conf

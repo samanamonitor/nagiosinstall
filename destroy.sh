d90c6959270b1b21bf2117ab56afb33358487116
Fabian Baena <fabbaena@gmail.com>
2018-05-31 12:07:52 -0400

    added comments to destroy

--- destroy.sh
+++ destroy.sh
@@ -9,6 +9,10 @@ die () {
 
 CONTAINER_NAME=nagiost1
 lxc-stop -n ${CONTAINER_NAME}
+echo "Container ${CONTAINER_NAME} stopped."
 lxc-destroy -n ${CONTAINER_NAME}
+echo "Container ${CONTAINER_NAME} destroyed."
 rm -f /etc/nginx/conf.d/${CONTAINER_NAME}.conf
+echo "NGINX reconfigured."
 systemctl restart nginx
+echo "Finished removing ${CONTAINER_NAME}"

0affc86e60223094e84c41ffeb638007a7a986e3
Fabian Baena <fabian.baena@samanagroup.co>
2019-05-10 10:52:15 -0400

    removed sudo from all commands

--- install_openstack.sh
+++ install_openstack.sh
@@ -13,7 +13,7 @@ resize_partition() {
     swapoff /dev/xvda5
     apt-get -y install parted
     mkdir /install_log
-    (echo d; echo 5; echo d; echo 2; echo d; echo n; echo p; echo 1; echo 2048; echo +7.5G; echo n; echo p; echo 2; echo 15706112; echo 16750591; echo a; echo 1; echo t; echo 2; echo 82; echo w) | sudo fdisk /dev/xvda
+    (echo d; echo 5; echo d; echo 2; echo d; echo n; echo p; echo 1; echo 2048; echo +7.5G; echo n; echo p; echo 2; echo 15706112; echo 16750591; echo a; echo 1; echo t; echo 2; echo 82; echo w) | fdisk /dev/xvda
     partprobe
     resize2fs /dev/xvda1
     mkswap /dev/xvda2 >> /install_log/swaplog.log
@@ -67,17 +67,17 @@ install_prereqs() {
 
     mkdir /tmp/install_log
     apt-get update
-    sudo apt-get install -y $INSTALL_PKGS >> /tmp/install_log/apt-get.log
-    sudo cpanm Number::Format
-    sudo cpanm Config::IniFiles
-    sudo cpam Date::Time
-    sudo cpanm DateTime
-    (echo y; echo y; echo y) | sudo sendmailconfig
+    apt-get install -y $INSTALL_PKGS >> /tmp/install_log/apt-get.log
+    cpanm Number::Format
+    cpanm Config::IniFiles
+    cpam Date::Time
+    cpanm DateTime
+    (echo y; echo y; echo y) | sendmailconfig
 
 
-    #curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-    #sudo bash -c "curl -s https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list"
-    #sudo apt-get update
+    #curl -s https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+    #bash -c "curl -s https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list"
+    #apt-get update
 
 }
 
@@ -95,53 +95,53 @@ install_wmi() {
 
 ##############donwload and install Nagios################
 install_nagios() {
-    sudo wget -P /tmp http://prdownloads.sourceforge.net/sourceforge/nagios/nagios-4.2.0.tar.gz 
-    sudo useradd nagios
-    sudo groupadd nagcmd
-    sudo usermod -a -G nagcmd nagios
-    sudo usermod -a -G nagios,nagcmd www-data
-    sudo tar zxvf /tmp/nagios-4.2.0.tar.gz -C /tmp
-    sudo tar zxvf /tmp/nagios-plugins-2.1.2.tar.gz -C /tmp
+    wget -P /tmp http://prdownloads.sourceforge.net/sourceforge/nagios/nagios-4.2.0.tar.gz 
+    useradd nagios
+    groupadd nagcmd
+    usermod -a -G nagcmd nagios
+    usermod -a -G nagios,nagcmd www-data
+    tar zxvf /tmp/nagios-4.2.0.tar.gz -C /tmp
+    tar zxvf /tmp/nagios-plugins-2.1.2.tar.gz -C /tmp
     cd /tmp/nagios-4.2.0
-    sudo ./configure --with-nagios-group=nagios --with-command-group=nagcmd
-    sudo make all
-    sudo make install
-    sudo make install-init
-    sudo make install-config
-    sudo make install-commandmode
-    sudo /usr/bin/install -c -m 644 sample-config/httpd.conf /etc/apache2/sites-available/nagios.conf
-    sudo ln -s /etc/apache2/sites-available/nagios.conf /etc/apache2/sites-enabled/
-    sudo cp -R contrib/eventhandlers/ /usr/local/nagios/libexec/
-    sudo /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
-    sudo a2enmod rewrite
-    sudo a2enmod cgi
-    sudo ln -s /etc/init.d/nagios /etc/rcS.d/S99nagios
-    sudo htpasswd -b -c /usr/local/nagios/etc/htpasswd.users nagiosadmin Samana81.
+    ./configure --with-nagios-group=nagios --with-command-group=nagcmd
+    make all
+    make install
+    make install-init
+    make install-config
+    make install-commandmode
+    /usr/bin/install -c -m 644 sample-config/httpd.conf /etc/apache2/sites-available/nagios.conf
+    ln -s /etc/apache2/sites-available/nagios.conf /etc/apache2/sites-enabled/
+    cp -R contrib/eventhandlers/ /usr/local/nagios/libexec/
+    /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
+    a2enmod rewrite
+    a2enmod cgi
+    ln -s /etc/init.d/nagios /etc/rcS.d/S99nagios
+    htpasswd -b -c /usr/local/nagios/etc/htpasswd.users nagiosadmin Samana81.
     ln -s /usr/local/nagios/etc /etc/nagios
 }
 
 ##############Configure nagios pluginss################
 install_nagios_plugins() {
-    sudo wget -P /tmp http://nagios-plugins.org/download/nagios-plugins-2.1.2.tar.gz
+    wget -P /tmp http://nagios-plugins.org/download/nagios-plugins-2.1.2.tar.gz
     cd /tmp/nagios-plugins-2.1.2
-    sudo ./configure --with-nagios-user=nagios --with-nagios-group=nagios
-    sudo make
-    sudo make install
+    ./configure --with-nagios-user=nagios --with-nagios-group=nagios
+    make
+    make install
 }
 
 ##############Configure pnp4nagios#####################
 install_pnp4nagios() {
-    sudo wget "https://sourceforge.net/projects/pnp4nagios/files/latest" -O /tmp/pnp4nagios.latest.tar.gz
-    sudo tar zxvf /tmp/pnp4nagios.latest.tar.gz -C /tmp
+    wget "https://sourceforge.net/projects/pnp4nagios/files/latest" -O /tmp/pnp4nagios.latest.tar.gz
+    tar zxvf /tmp/pnp4nagios.latest.tar.gz -C /tmp
     cd /tmp/pnp4nagios-0.6.26
-    sudo ./configure --with-nagios-user=nagios --with-nagios-group=nagcmd
-    sudo make all
-    sudo make fullinstall
-    sudo mv /etc/httpd/conf.d/pnp4nagios.conf /etc/apache2/sites-available/
-    sudo rm -Rf /etc/httpd
-    sudo ln -s /etc/apache2/sites-available/pnp4nagios.conf /etc/apache2/sites-enabled/
-    sudo mv /usr/local/pnp4nagios/share/install.php /usr/local/pnp4nagios/share/install-old.php
-    sudo ln -s /etc/init.d/npcd /etc/rcS.d/S98npcd
+    ./configure --with-nagios-user=nagios --with-nagios-group=nagcmd
+    make all
+    make fullinstall
+    mv /etc/httpd/conf.d/pnp4nagios.conf /etc/apache2/sites-available/
+    rm -Rf /etc/httpd
+    ln -s /etc/apache2/sites-available/pnp4nagios.conf /etc/apache2/sites-enabled/
+    mv /usr/local/pnp4nagios/share/install.php /usr/local/pnp4nagios/share/install-old.php
+    ln -s /etc/init.d/npcd /etc/rcS.d/S98npcd
 }
 
 install_check_bw() {
@@ -170,7 +170,7 @@ install_mibs() {
     #   SNMP-TARGET-MIB.txt
     #   SNMP-VIEW-BASED-ACM-MIB.txt
 
-    #sudo wget http://192.168.0.12/support/docs/snmp/HP-Openview/NS-MIB-smiv2.mib -O /usr/share/snmp/mibs/NS-MIB-smiv2.txt
+    #wget http://192.168.0.12/support/docs/snmp/HP-Openview/NS-MIB-smiv2.mib -O /usr/share/snmp/mibs/NS-MIB-smiv2.txt
     echo "mibs ALL" >> /etc/snmp/snmp.conf
 }
 
@@ -184,18 +184,18 @@ install_pynag() {
 install_check_mssql() {
     cp $DIR/support/check_mssql ${NAGIOS_LIBEXEC}
     chown nagios.nagios ${NAGIOS_LIBEXEC}/check_mssql
-    #sudo ACCEPT_EULA=Y apt-get -y install msodbcsql17 mssql-tools
-    #sudo apt-get -y install unixodbc-dev
-    #sudo apt install -y php7.0-dev
-    #sudo pecl install sqlsrv-5.3.0
-    #sudo pecl install pdo_sqlsrv-5.3.0
-    #cat <<EOF | sudo tee /etc/php/7.0/mods-available/pdo_sqlsrv.ini
+    #ACCEPT_EULA=Y apt-get -y install msodbcsql17 mssql-tools
+    #apt-get -y install unixodbc-dev
+    #apt install -y php7.0-dev
+    #pecl install sqlsrv-5.3.0
+    #pecl install pdo_sqlsrv-5.3.0
+    #cat <<EOF | tee /etc/php/7.0/mods-available/pdo_sqlsrv.ini
     #; configuration for php sqlite3 module
     #; priority=30
     #extension=pdo_sqlsrv.so
     #EOF
-    #sudo ln -s /etc/php/7.0/mods-available/pdo_sqlsrv.ini /etc/php/7.0/apache2/conf.d/20-pdo_sqlsrv.ini
-    #sudo ln -s /etc/php/7.0/mods-available/pdo_sqlsrv.ini /etc/php/7.0/cli/conf.d/20-pdo_sqlsrv.ini
+    #ln -s /etc/php/7.0/mods-available/pdo_sqlsrv.ini /etc/php/7.0/apache2/conf.d/20-pdo_sqlsrv.ini
+    #ln -s /etc/php/7.0/mods-available/pdo_sqlsrv.ini /etc/php/7.0/cli/conf.d/20-pdo_sqlsrv.ini
     sed -i '/;^\s\+tds\s\+version/a    tds version = 8.0' \
         /etc/freetds/freetds.conf
 }

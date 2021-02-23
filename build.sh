bfd5259b88b310cdbd83b12af8f1fa268ea5c07f
Fabian Baena <fabian.baena@samanagroup.co>
2021-02-23 10:06:00 -0500

    fix container name issue

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -xe
 
 apt -y install docker.io
 docker build -t samanamon:v1 https://github.com/samanamonitor/nagiosinstall.git
-docker save samanamonitor | gzip > samanamonitor.tgz
+docker save samanamon:v1 | gzip > samanamonitor.tgz
 # upload samanamonitor.tgz to S3 and configure permissions

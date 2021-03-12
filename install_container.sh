08e3547789168b6ee834c0c88610b1d33a3456b0
Fabian Baena <fabian.baena@samanagroup.co>
2021-03-12 15:02:56 -0500

    simplify code

--- install_container.sh
+++ install_container.sh
@@ -32,55 +32,33 @@ else
     exit 1
 fi
 
+add-local-volume() {
+    VOLNAME=$1
+    VOLPATH=$2
+    if ! docker volume inspect $VOLNAME > /dev/null 2>&1; then
+        docker volume create $VOLNAME
+    fi
+    if [ -L $VOLPATH ]; then
+        rm $VOLPATH
+    fi
+    ln -s $(docker inspect $VOLNAME | jq -r .[0].Mountpoint) $VOLPATH
+
+}
+
 if ! docker image inspect $IMAGE > /dev/null 2>&1; then
     wget -O - ${IMAGE_URL} | docker load
 fi
 
-if [ ! -d /usr/local/nagios ]; then
-    mkdir -p /usr/local/nagios
-fi
-if [ ! -d /usr/local/pnp4nagios ]; then
-    mkdir -p /usr/local/pnp4nagios
-fi
-
-if ! docker volume inspect nagios_etc > /dev/null 2>&1; then
-    docker volume create nagios_etc
-fi
-if [ -L /usr/local/nagios/etc ]; then
-    rm /usr/local/nagios/etc
-fi
-ln -s $(docker inspect nagios_etc | jq -r .[0].Mountpoint) /usr/local/nagios/etc
-
-if ! docker volume inspect nagios_libexec > /dev/null 2>&1; then
-    docker volume create nagios_libexec
-fi
-if [ -L /usr/local/nagios/libexec ]; then
-    rm /usr/local/nagios/libexec
-fi
-ln -s $(docker inspect nagios_libexec | jq -r .[0].Mountpoint) /usr/local/nagios/libexec
-
-if ! docker volume inspect pnp4nagios_perfdata > /dev/null 2>&1; then
-    docker volume create pnp4nagios_perfdata
-fi
-if [ -L /usr/local/pnp4nagios/perfdata ]; then
-    rm /usr/local/pnp4nagios/perfdata
-fi
-ln -s $(docker inspect pnp4nagios_perfdata | jq -r .[0].Mountpoint) /usr/local/pnp4nagios/perfdata
-
-if ! docker volume inspect ssmtp_etc > /dev/null 2>&1; then
-    docker volume create ssmtp_etc
-fi
-if [ ! -d /usr/local/ssmtp ]; then
-    mkdir -p /usr/local/ssmtp
-fi
-if [ -L /usr/local/ssmtp/etc ]; then
-    rm /usr/local/ssmtp/etc
-fi
-ln -s $(docker volume inspect ssmtp_etc | jq -r .[0].Mountpoint) /usr/local/ssmtp/etc
+add-local-volume nagios_etc /usr/local/nagios/etc
+add-local-volume nagios_libexec /usr/local/nagios/libexec
+add-local-volume nagios_var /usr/local/nagios/var
+add-local-volume pnp4nagios_perfdata /usr/local/pnp4nagios/var/perfdata
+add-local-volume ssmtp_etc /usr/local/ssmtp/etc
 
 docker run -p 80:80 -p 443:443 -p 2379:2379 \
     --mount source=nagios_etc,target=/usr/local/nagios/etc \
     --mount source=nagios_libexec,target=/usr/local/nagios/libexec \
+    --mount source=nagios_var,target=/usr/local/nagios/var \
     --mount source=pnp4nagios_perfdata,target=/usr/local/pnp4nagios/var/perfdata \
     --mount source=ssmtp_etc,target=/etc/ssmtp \
     --name sm -it -d $IMAGE /bin/bash -x /start.sh

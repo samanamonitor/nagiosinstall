2fdb22b1c863d816be5968829c543c0bd21b1460
Fabian Baena <fabian.baena@samanagroup.co>
2021-04-14 10:01:29 -0400

    separate build process from image creation

---
+++ build_wmi/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# run the following commands on windows for winRM to be enabled
+# winrm quickconfig -transport:https
+LIBS="git build-essential autoconf python"
+local TEMPDIR=$(mktemp -d)
+local CURDIR=$(pwd)
+apt update
+apt install -y $LIBS
+git clone https://github.com/samanamonitor/wmi.git ${TEMPDIR}
+cd ${TEMPDIR}
+ulimit -n 100000 && make "CPP=gcc -E -ffreestanding"
+install ${TEMPDIR}/Samba/source/bin/wmic ${WMIC_PATH}/
+cd ${CURDIR}
+rm -Rf ${TEMPDIR}

7e0df9a23ebe675b844ca494afac3dc67fd826af
Fabian Baena <fabian.baena@samanagroup.co>
2021-04-21 09:39:12 -0400

    fix etcd bug

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/local/nagios/bin/nagios /etc/nagios/nagios.cfg &
+/usr/local/nagios/bin/nagios /etc/nagios/nagios.cfg  >> /usr/local/nagios/var/log/nagios-service.log 2>&1 &
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start Nagios: $status"
@@ -24,7 +24,12 @@ if [ $status -ne 0 ]; then
   exit $status
 fi
 
-ETCD_ADVERTISE_CLIENT_URLS=http://0.0.0.0:2379 ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379 ETCD_DATA_DIR="/var/lib/etcd/default" /usr/bin/etcd &
+ETCD_ADVERTISE_CLIENT_URLS=http://0.0.0.0:2379 \
+    ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379 \
+    ETCD_DATA_DIR="/var/lib/etcd/default" \
+    ETCD_MAX_SNAPSHOTS=1 \
+    ETCD_MAX_WALS=1 \
+    /usr/bin/etcd &
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start ETCD: $status"

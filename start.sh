38cd55bd57cce613ebb9649587b4b86062b51dae
Fabian Baena <fabian.baena@samanagroup.co>
2022-10-13 11:32:52 -0400

    remove pnp4nagios and replace with graphite

--- start.sh
+++ start.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-/etc/init.d/nagios start
-/etc/init.d/apache2 start
-
-/usr/local/pnp4nagios/bin/npcd -f /usr/local/pnp4nagios/etc/npcd.cfg &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start NPCD: $status"
-  exit $status
-fi
-
-/bin/bash
+service nagios start
+service apache2 start
+service graphios start
+tail -f /usr/local/nagios/var/nagios.log

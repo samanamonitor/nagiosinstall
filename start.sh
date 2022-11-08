8b74e7ce6b77885e60d22b637e7daaec1e856289
Fabian Baena <fabbaena@gmail.com>
2022-11-08 12:10:07 -0500

    added start retries

--- start.sh
+++ start.sh
@@ -1,8 +1,49 @@
 #!/bin/bash
 
-service nagios start
-sleep 5
-service apache2 start
-sleep 5
+RETRIES=2
+
+rm /run/nagios.lock /run/apache2/apache2.pid /run/sammworker_process.pid
+r=$RETRIES
+while [ $r -gt 0 ]; do
+    service nagios start
+    sleep 5
+    if [ -f /run/nagios.lock ]; then
+        break
+    else
+        echo "Retrying Nagios..."
+        r=$(( $r - 1 ))
+    fi
+    if [ "$r" == "0" ]
+        echo "Nagios didn't start. Abort" >&2
+        exit 1
+    fi
+done
+
+r=$RETRIES
+while [ $r -gt 0 ]; do
+    service apache2 start
+    sleep 5
+    if [ -f /run/apache2/apache2.pid ]; then
+        break
+    else
+        echo "Retrying Apache..."
+        r=$(( $r - 1 ))
+    fi
+    if [ "$r" == "0" ]
+        echo "Apache didn't start. Abort" >&2
+        exit 1
+    fi
+done
+
 service graphios start
+sleep 5
+
+if [ -x /usr/local/nagios/libexec/sammworker.py ]; then
+    /usr/local/nagios/libexec/sammworker.py
+    sleep 5
+    if [ ! -f /run/sammworker_process.pid ]; then
+        echo "SAMM Worker didn't start. Continuing." >&2
+    fi
+fi
+
 tail -f /usr/local/nagios/var/nagios.log

88a19a57672fd0b8ff6dc825391692b5fbb55f13
Fabian Baena <fabbaena@gmail.com>
2022-11-09 11:48:03 -0500

    fix bug

--- editini.py
+++ editini.py
@@ -11,15 +11,15 @@ def usage():
 
 if len(sys.argv) < 3:
     usage()
-filename=argv[1]
-action=argv[2]
+filename=sys.argv[1]
+action=sys.argv[2]
 if (action == 'set' and len(sys.argv) != 6) or \
         (action == 'get' and len(sys.argv) != 5):
     usage()
 
-section = argv[3]
-option = argv[4]
-value = argv[5]
+section = sys.argv[3]
+option = sys.argv[4]
+value = sys.argv[5]
 with open(filename, "r+") as f:
     config.readfp(f)
     config.set(section, option, value)
